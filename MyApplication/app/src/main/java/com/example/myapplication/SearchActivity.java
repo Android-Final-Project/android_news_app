d0bbb87b186893facbaea470b2a4b3a82026ca6c
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2024-03-14 08:06:21 -0400

    Last update

--- MyApplication/app/src/main/java/com/example/myapplication/SearchActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = findViewById(R.id.search_view);
+        ImageView searchIcon = (ImageView) searchView.findViewById(androidx.appcompat.R.id.search_button);
+        if (searchIcon != null) {
+            Drawable wrappedDrawable = DrawableCompat.wrap(searchIcon.getDrawable());
+            DrawableCompat.setTint(wrappedDrawable, ContextCompat.getColor(this, R.color.navy_blue));
+            searchIcon.setImageDrawable(wrappedDrawable);
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
+
+
+
 }
