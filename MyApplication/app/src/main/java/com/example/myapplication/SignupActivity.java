08d5332a311a6fc6026e9edd5296c130c33184e9
Bruno Landeiro <landeiro.b@gmail.com>
2024-03-16 20:40:42 -0400

    Signup with firebase auth

--- MyApplication/app/src/main/java/com/example/myapplication/SignupActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -1,11 +1,21 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -13,11 +23,14 @@ public class SignupActivity extends AppCompatActivity {
     private Button continueButton;
     private ImageView backButton;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mAuth = FirebaseAuth.getInstance();
         fullNameInput = findViewById(R.id.fullnameinput);
         usernameInput = findViewById(R.id.usernameinput);
         passwordInput = findViewById(R.id.passwordinput);
@@ -60,15 +73,29 @@ public class SignupActivity extends AppCompatActivity {
         // For example, send a network request to your server's sign-up endpoint.
         // This is just a placeholder and will not actually send a network request.
         String fullName = fullNameInput.getText().toString();
+        // TODO: Replace the username for email (FireBase Auth)
         String username = usernameInput.getText().toString();
         String password = passwordInput.getText().toString();
 
-        // TODO: Replace the below toast message with actual network request logic
-        Toast.makeText(this, "Account created for: " + fullName, Toast.LENGTH_SHORT).show();
 
-        // After successful sign-up, navigate the user to the login screen or main activity
-        // Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-        // startActivity(intent);
-        // finish(); // Finish this activity so the user can't return to it
+        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignupActivity.this, "Account created for: " + fullName, Toast.LENGTH_SHORT).show();
+                     Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                     startActivity(intent);
+                     finish(); // Finish this activity so the user can't return to it
+                } else {
+                    String errorMessage = "Something went wrong.";
+                    if(Objects.nonNull(task.getException())) {
+                        errorMessage = task.getException().getMessage();
+                    }
+                    Toast.makeText(SignupActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
